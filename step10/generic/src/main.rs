e548a22e46f2404bea443108add04929e17bd6b8
pyjun01 <pyjun02@gmail.com>
2022-05-05 00:47:48 +0900

    step10

---
+++ step10/generic/src/main.rs
@@ -0,0 +1,29 @@
+// fn largest<T>(list: &[T]) -> T {
+//     let mut largest = list[0];
+
+//     for &item in list.iter() {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+
+//     largest;
+// }
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+
+fn main() {
+    let a = Point { x: 10, y: 10 };
+
+    println!("{}", a.x());
+}
